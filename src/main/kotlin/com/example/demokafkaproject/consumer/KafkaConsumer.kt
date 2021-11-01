de167409d7e36813097c5acbd87b4408b1fb6fa8
d.veselkov <d.veselkov@crpt.ru>
2021-11-01 22:54:37 +0300

    initial

--- src/main/kotlin/com/example/demokafkaproject/consumer/KafkaConsumer.kt
+++ src/main/kotlin/com/example/demokafkaproject/consumer/KafkaConsumer.kt
@@ -1,17 +1,17 @@
 package com.example.demokafkaproject.consumer
 
 import com.example.demokafkaproject.config.KafkaProperties
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.kafka.annotation.KafkaHandler
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 
 @Service
 class KafkaConsumer(private var kafkaProperties: KafkaProperties) {
 
-    @KafkaListener(topics = [kafkaProperties.topicName])
-    fun listenKafka(msg:String){
-
+    @KafkaListener(
+        topics = ["\${kafka.topic-name}"],
+        groupId = "\${kafka.group_id}"
+    )
+    fun listenKafka(msg: String) {
+        System.out.println("Received Message in group foo: $msg");
     }
-
 }
